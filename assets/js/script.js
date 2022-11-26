78cdbacaee1f3aaf8b4e498c4d128f8bc91de007
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2022-11-26 13:35:26 -0800

    first update

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"];
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numberArray = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharactersArray = ["!","#","$","%","&","*","+", "-","/",":",";","<","=","]",">","?","@","[","^"];
+
+
+function generatePassword(){
+    var passwordLength = parseInt(prompt("How many characters would you like your password to contain?", "8 - 128 characters"));
+    var password = '';
+    var combinedArr = [];
+    
+    if(!(passwordLength >= 8 && passwordLength <= 128)){
+        alert('Must be');
+        return;
+    }
+
+    var includesNumber = confirm("Click OK to confirm including numeric characters.");
+    var includesSpecialCharaters = confirm("Click OK to confirm including special characters.");
+    var includesUppercase = confirm("Click OK to confirm including uppercase.")  ;
+    var includesLowercase = confirm("Click OK to confirm including lowercase.")  ;
+
+    if(!includesNumber && !includesSpecialCharaters && !includesUppercase && !includesLowercase){
+        alert('Must be');
+        return;
+    };
+
+};
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
